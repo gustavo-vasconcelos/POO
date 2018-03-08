045c75ab66f110a300b67fe97140dfe886bfa8b3
Gustavo Vasconcelos <gustavofamoso48@gmail.com>
2018-03-08 21:15:39 +0000

    Ficheiro .css adicionado, script modificado

--- Ficha_01/script.js
+++ Ficha_01/script.js
@@ -1,16 +1,32 @@
+function output(texto){
+    let elemento = document.getElementById('output');
+    elemento.innerHTML = texto;
+}
+
+function adicionarElementos(texto){
+    let paragrafo = document.createElement("p")
+    let node = document.createTextNode(texto)
+    paragrafo.appendChild(node)
+    let elemento = document.getElementById('console')
+    elemento.appendChild(paragrafo)
+}
+
 function helloWorld() {
     console.log("Olá mundo!")
+    output("Olá mundo!")
 }
 
 function helloUser(){
     let nome = prompt("Insira o seu nome:")
-    console.log("Olá " + nome)
+    console.log("Olá " + nome + "!")
+    output("Olá " + nome + "!")
 }
 
 function calcularArea(){
     let comprimento = parseInt(prompt("Insira o comprimento:"))
     let largura = parseInt(prompt("Insira a largura:"))
     console.log("Área = " + comprimento*largura + " m.")
+    output("Área = " + comprimento*largura + " m.")
 }
 
 function calculadora(){
@@ -20,23 +36,29 @@ function calculadora(){
     
     switch(operacao){
         case "+":
-            console.log("Resultado: " + num1 + " + " + num2 + " = " + num1+num2)
+            console.log("Resultado: " + num1 + " + " + num2 + " = " + (num1+num2))
+            output("Resultado: " + num1 + " + " + num2 + " = " + (num1+num2))
             break
         case "-":
-            console.log("Resultado: " + num1 + " - " + num2 + " = " + num1-num2)
+            console.log("Resultado: " + num1 + " - " + num2 + " = " + (num1-num2))
+            output("Resultado: " + num1 + " - " + num2 + " = " + (num1-num2))
             break
         case "*":
-            console.log("Resultado: " + num1 + " x " + num2 + " = " + num1*num2)
+            console.log("Resultado: " + num1 + " x " + num2 + " = " + (num1*num2))
+            output("Resultado: " + num1 + " x " + num2 + " = " + (num1*num2))
             break
         case "/":
             if(num2 != 0){
-                console.log("Resultado: " + num1 + " / " + num2 + " = " + num1/num2)
+                console.log("Resultado: " + num1 + " / " + num2 + " = " + (num1/num2))
+                output("Resultado: " + num1 + " / " + num2 + " = " + (num1/num2))
             }else{
                 console.log("Impossível dividir por 0!")
+                output("Impossível dividir por 0!")
             }
             break
         default:
             console.log("Operação inválida!")
+            output("Operação inválida!")
             break
     }
 }
@@ -45,28 +67,45 @@ function imc(){
     let altura = parseInt(prompt("Insira a sua altura em metros:"))
     let peso = parseInt(prompt("Insira a sua massa em kg"))
     let imc = peso / (altura * altura)
-
-    if(imc < 18.5){
-        console.log("Está abaixo do peso.")
-    }else if(imc <= 24.9){
-        console.log("Está com peso normal.")        
-    }else if(imc <= 29.9){
-        console.log("Está com sobrepeso.")        
-    }else if(imc <= 34.9){
-        console.log("Está com obesidade grau I.")        
-    }else if(imc <= 39.9){
-        console.log("Está com obesidade grau II.")        
+    if(imc == NaN){
+        if(imc < 18.5){
+            console.log("Está abaixo do peso.")
+            output("Está abaixo do peso.")
+        }else if(imc <= 24.9){
+            console.log("Está com peso normal.")        
+            output("Está com peso normal.")
+        }else if(imc <= 29.9){
+            console.log("Está com sobrepeso.")        
+            output("Está com sobrepeso.")
+        }else if(imc <= 34.9){
+            console.log("Está com obesidade grau I.")    
+            output("Está com obesidade grau I.")    
+        }else if(imc <= 39.9){
+            console.log("Está com obesidade grau II.") 
+            output("Está com obesidade grau II.")       
+        }else{
+            console.log("Está com obesidade grau III.")          
+            output("Está com obesidade grau III.")      
+        }
     }else{
-        console.log("Está com obesidade grau III.")                
+        console.log("Insira os campos corretamente!")
+        output("Insira os campos corretamente!")
     }
 }
 
 function eco(){
     let string = prompt("Escreva algo:")
     let vezes = parseInt(prompt("Deseja repetir quantas vezes?"))
-    for(let i=0; i<vezes; i++){
-        console.log(string)
+    if(vezes == NaN){
+        for(let i=0; i<vezes; i++){
+            console.log(string)
+        }
+        output("(" + vezes + ") " + string)
+    }else{
+        console.log("Insira os campos corretamente!")
+        output("Insira os campos corretamente!")
     }
+
 }
 
 function valoresIntervalo(){
@@ -74,6 +113,7 @@ function valoresIntervalo(){
     let num2 = parseInt(prompt("Insira o segundo valor:"))
     for(let i=num1; i<num2; i++){
         console.log(i)
+        adicionarElementos(i)
     }
 }
 
@@ -81,6 +121,7 @@ function tabuada(){
     let num = parseInt(prompt("Qual a tabuada desejada?"))
     for(let i=1; i<=10; i++){
         console.log(num + " x " + i + " = " + num*i)
+        adicionarElementos(num + " x " + i + " = " + num*i)
     }
 }
 
@@ -93,7 +134,9 @@ function multiplos3(){
             soma += i
         }
     }
-    console.log(soma)
+    console.log("Soma dos múltiplos de 3 no intervalo [" + num1 + ", " + num2 + "[ = " + soma)
+    output("Soma dos múltiplos de 3 no intervalo [" + num1 + ", " + num2 + "[ = " + soma)
+
 }
 
 function primo(){
@@ -106,8 +149,10 @@ function primo(){
     }
     if(divisores <= 2 && num != 1){
         console.log("Primo.")
+        output("Primo.")
     }else{
         console.log("Não primo.")
+        output("Não primo.")
     }
 }
 
@@ -117,46 +162,91 @@ function fatorial(){
     for(let i=1; i<=num; i++){
         fatorial *= i
     }
-    console.log(fatorial)
+    console.log(num + "! = " + fatorial)
+    output(num + "! = " + fatorial)
 }
 
 function perfeito(){
     let num = parseInt(prompt("Insira um número:"))
+    let divisores = [];
     let soma = 0
     for(let i=1; i<num; i++){
         if(num % i == 0){
             soma += i
+            divisores.push(i)
         }
     }
     if(soma == num){
+        for(let i=0; i<divisores.length - 1; i++){
+            adicionarElementos(divisores[i])
+        }
+        adicionarElementos("+ " + divisores[divisores.length - 1])
+        adicionarElementos("---------------")
+        adicionarElementos(num)
         console.log("Número perfeito.")
+        output("Número perfeito:")
     }else{
         console.log("Não é perfeito.")
+        output("Não é perfeito.")
     }
 }
 
-function bissexto(){
+function bissexto(){    
+    let bissexto = false
+    let ano = parseInt(prompt("Insira o ano desejado:"))
+    if(ano % 4 == 0 && ano % 100 == 0 && ano % 400 == 0){
+        bissexto = true
+    }else if(ano % 4 == 0 && ano % 100 == 0){
+        bissexto = false
+    }else if(ano % 4 == 0){
+        bissexto = true
+    }
 
+    if(bissexto){
+        console.log("O ano " + ano + " é bissexto.")
+        output("O ano " + ano + " é bissexto.")
+    }else{
+        console.log("O ano " + ano + " não é bissexto.")
+        output("O ano " + ano + " é bissexto.")
+    }
 }
 
 function capicua(){
+    let num
+    do{
+        num = prompt("Insira um número entre 100 e 999 (inclusive):")
+    } while(num < 100 || num > 999)
 
+    if(num.charAt(0) == num.charAt(2)){
+        console.log("O número " + num + " é capicua.")
+        output("O número " + num + " é capicua.")
+    }else{
+        console.log("O número " + num + " não é capicua.")
+        output("O número " + num + " não é capicua.")
+    }
 }
 
 function advinha(){
     let aleatorio = Math.floor(Math.random(1) * 100)
     console.log(aleatorio)
-    let tentativas = 0
-    let num = 0
+    let tentativas = 1
+    let num
     do {
-        num = parseInt(prompt("Insira um valor (" + (5-tentativas) + " tentativas restantes):"))
+        num = parseInt(prompt("Insira um valor (" + (6-tentativas) + " tentativas restantes):"))
         if(num > aleatorio){
             console.log("Insira um valor menor!")
-        }
-        if(num < aleatorio){
+            alert("Insira um valor menor!")
+        }else if(num < aleatorio){
             console.log("Insira um valor maior!")
+            alert("Insira um valor maior!")
+        }else{
+            console.log("Parabéns, advinhou o número! Tentativas utilizadas: " + tentativas)
+            output("Parabéns, advinhou o número! Tentativas utilizadas: " + tentativas)
         }
         tentativas++
-    } while (num != aleatorio && tentativas >= 5)
-
+    } while (num != aleatorio && tentativas <= 5)
+    if(tentativas > 5 && num != aleatorio){
+        console.log("GAME OVER! Esgotou o número de tentativas. Tente novamente.")
+        output("GAME OVER! Esgotou o número de tentativas. Tente novamente.")
+    }
 }
