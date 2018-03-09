e04e46cea29e2130fd1dea133731890dbf6e1e5f
Gustavo Vasconcelos <gustavofamoso48@gmail.com>
2018-03-09 20:23:54 +0000

    Falta a alínea n (ficha 2)

--- Ficha_02/index.js
+++ Ficha_02/index.js
@@ -56,8 +56,6 @@ function concatenarArrays(){
     let string = ""
     for(let i=0; i < tamanho; i++){
         array[i] = prompt("Insira o " + (i+1) + "º elemento:")
-    }
-    for(let i in array){
         string += array[i]
     }
     console.log("Elementos do array concatenados: " + string)
@@ -100,4 +98,102 @@ function maiorString(){
     }
     console.log("Maior string: " + maiorElemento)
     output("Maior string: " + maiorElemento)
+}
+
+function numeroOcorrencias(){
+    let tamanho = parseInt(prompt("Insira o tamanho do array:"))
+    let array = []
+    for(let i=0; i < tamanho; i++){
+        array[i] = prompt("Insira o " + (i+1) + "º elemento:")
+    }
+    let string = prompt("Indique o elemento a procurar:")
+    let ocorrencias = 0
+
+    for(let i in array){
+        if(array[i] == string){
+            ocorrencias++
+        }
+    }
+    console.log("Número de ocorrências de " + string + " no array: " + ocorrencias + ".")
+    output("Número de ocorrências de " + string + " no array: " + ocorrencias + ".")
+}
+
+function password(){
+    let array = []
+    array[0] = prompt("Insira a password:")
+    array[1] = prompt("Confirme a password:")
+    if(array[0] == array[1]){
+        console.log("Match!")
+        output("Match!")
+    }else{
+        console.log("No match!")
+        output("No match!")
+    }
+}
+
+function returnElemento(){
+    let tamanho = parseInt(prompt("Insira o tamanho do array:"))
+    let array = []
+    for(let i=0; i < tamanho; i++){
+        array[i] = prompt("Insira o " + (i+1) + "º elemento:")
+    }
+    let num = parseInt(prompt("Indique a posição do elemento a retornar:"))
+    console.log(num + "º posição do array: " + array[num] + ".")
+    output(num + "º posição do array: " + array[num] + ".")
+}
+
+function numerosPositivos(){
+    let num = parseInt(prompt("Insira um número:"))
+    let array = []
+    for(let i=0; i<num - 1; i++){
+        array[i] = i+1
+    }
+    console.log("Números no intervalo [1, " + num + "[ = " + array + ".")
+    output("Números no intervalo [1, " + num + "[ = " + array + ".")
+}
+
+function numerosPares(){
+    let tamanho = parseInt(prompt("Insira o tamanho do array:"))
+    let array = []
+    let array2 = []
+    for(let i=0; i < tamanho; i++){
+        array[i] = parseInt(prompt("Insira o " + (i+1) + "º elemento:"))
+        if(array[i] % 2 == 0){
+            array2.push(array[i])
+        }
+    }
+    console.log("Array com elementos pares apenas: " + array2 + ".")
+    output("Array com elementos pares apenas: " + array2 + ".")
+}
+
+function incrementar1(){
+    let tamanho = parseInt(prompt("Insira o tamanho do array:"))
+    let array = []
+    for(let i=0; i < tamanho; i++){
+        array[i] = parseInt(prompt("Insira o " + (i+1) + "º elemento:")) + 1
+    }
+    console.log("Incrementado 1: " + array)
+    output("Incrementado 1: " + array)
+}
+
+
+//INCOMPLETO
+function somarArrays(){
+    let tamanho = parseInt(prompt("Insira o tamanho do primeiro array:"))
+    let array = []
+    for(let i=0; i < tamanho; i++){
+        array[i] = parseInt(prompt("Insira o " + (i+1) + "º elemento do primeiro array:")) + 1
+    }
+
+    let tamanho2 = parseInt(prompt("Insira o tamanho do segundo array:"))
+    let array2 = []
+    for(let i=0; i < tamanho2; i++){
+        array2[i] = parseInt(prompt("Insira o " + (i+1) + "º elemento do segundo array:")) + 1
+    }
+
+    let soma = array2.map(function (num, indx){
+        return num + array[indx]
+    })
+    
+    console.log(soma)
 }
