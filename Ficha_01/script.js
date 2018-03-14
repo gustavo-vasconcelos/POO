3760da2cb071aa6836d7e3113522ed641ff77c97
Gustavo Vasconcelos <gustavofamoso48@gmail.com>
2018-03-14 21:55:28 +0000

    Atualização do script

--- Ficha_01/script.js
+++ Ficha_01/script.js
@@ -1,6 +1,13 @@
 function output(texto){
+    console.log(texto)
     let elemento = document.getElementById('output');
-    elemento.innerHTML = texto;
+    elemento.innerHTML += texto + "\n";
+}
+
+function outputMesmaLinha(texto){
+    console.log(texto)
+    let elemento = document.getElementById('output');
+    elemento.innerHTML += texto + " ";
 }
 
 function adicionarElementos(texto){
@@ -11,126 +18,178 @@ function adicionarElementos(texto){
     elemento.appendChild(paragrafo)
 }
 
+/*
+    ALÍNEA A
+*/
+
 function helloWorld() {
-    console.log("Olá mundo!")
     output("Olá mundo!")
 }
 
+/*
+    ALÍNEA B
+*/
+
 function helloUser(){
     let nome = prompt("Insira o seu nome:")
-    console.log("Olá " + nome + "!")
-    output("Olá " + nome + "!")
+    output(`Olá, ${nome}!`)
 }
 
-function calcularArea(){
+/*
+    ALÍNEA C
+*/
+
+function alineaC(){
     let comprimento = parseInt(prompt("Insira o comprimento:"))
     let largura = parseInt(prompt("Insira a largura:"))
-    console.log("Área = " + comprimento*largura + " m.")
-    output("Área = " + comprimento*largura + " m.")
+    output(`Área = ${calcularArea(comprimento, largura)}m.`)
 }
 
-function calculadora(){
+function calcularArea(comprimento, largura){
+    return comprimento*largura
+}
+
+/*
+    ALÍNEA D
+*/
+
+function alineaD(){
     let num1 = parseInt(prompt("Insira o primeiro valor:"))
     let num2 = parseInt(prompt("Insira o segundo valor:"))
     let operacao = prompt("Qual a operação? (+ | - | * | /)")
-    
+    output(calculadora(num1, num2, operacao))
+}
+
+function calculadora(num1, num2, operacao){
     switch(operacao){
         case "+":
-            console.log("Resultado: " + num1 + " + " + num2 + " = " + (num1+num2))
-            output("Resultado: " + num1 + " + " + num2 + " = " + (num1+num2))
+            return(`Resultado: ${num1} + ${num2} = ${(num1+num2)}`)
             break
         case "-":
-            console.log("Resultado: " + num1 + " - " + num2 + " = " + (num1-num2))
-            output("Resultado: " + num1 + " - " + num2 + " = " + (num1-num2))
+            return(`Resultado: ${num1} - ${num2} = ${(num1-num2)}`)
             break
         case "*":
-            console.log("Resultado: " + num1 + " x " + num2 + " = " + (num1*num2))
-            output("Resultado: " + num1 + " x " + num2 + " = " + (num1*num2))
+            return(`Resultado: ${num1} x ${num2} = ${(num1*num2)}`)
             break
         case "/":
             if(num2 != 0){
-                console.log("Resultado: " + num1 + " / " + num2 + " = " + (num1/num2))
-                output("Resultado: " + num1 + " / " + num2 + " = " + (num1/num2))
+                return(`Resultado: ${num1} / ${num2} = ${(num1/num2)}`)
             }else{
-                console.log("Impossível dividir por 0!")
-                output("Impossível dividir por 0!")
+                return("Impossível dividir por 0!")
             }
             break
         default:
-            console.log("Operação inválida!")
-            output("Operação inválida!")
+            return("Operação inválida!")
             break
     }
 }
 
-function imc(){
-    let altura = parseInt(prompt("Insira a sua altura em metros:"))
-    let peso = parseInt(prompt("Insira a sua massa em kg"))
+/*
+    ALÍNEA E
+*/
+
+function alineaE(){
+    let altura = parseFloat(prompt("Insira a sua altura em metros:"))
+    let peso = parseFloat(prompt("Insira a sua massa em kg"))
+    let resultado = (isNaN(altura) || isNaN(peso)) ? "Insira valores numéricos!" : imc(peso, altura)
+    output(resultado)
+}
+
+function imc(peso, altura){
     let imc = peso / (altura * altura)
-    console.log(imc)
     if(imc < 18.5){
-        console.log("Está abaixo do peso.")
-        output("Está abaixo do peso.")
+        return("Está abaixo do peso.")
     }else if(imc <= 24.9){
-        console.log("Está com peso normal.")        
-        output("Está com peso normal.")
+        return("Está com peso normal.")
     }else if(imc <= 29.9){
-        console.log("Está com sobrepeso.")        
-        output("Está com sobrepeso.")
+        return("Está com sobrepeso.")
     }else if(imc <= 34.9){
-        console.log("Está com obesidade grau I.")    
-        output("Está com obesidade grau I.")    
+        return("Está com obesidade grau I.")    
     }else if(imc <= 39.9){
-        console.log("Está com obesidade grau II.") 
-        output("Está com obesidade grau II.")       
+        return("Está com obesidade grau II.")       
     }else{
-        console.log("Está com obesidade grau III.")          
-        output("Está com obesidade grau III.")      
+        return("Está com obesidade grau III.")      
     } 
 }
 
-function eco(){
+/*
+    ALÍNEA F
+*/
+
+function alineaF(){
     let string = prompt("Escreva algo:")
     let vezes = parseInt(prompt("Deseja repetir quantas vezes?"))
+    eco(string, vezes)
+}
+
+function eco(string, vezes){
     for(let i=0; i<vezes; i++){
-        console.log(string)
+        output(string)
     }
-    output("(" + vezes + ") " + string)
-
 }
 
-function valoresIntervalo(){
+/*
+    ALÍNEA G
+*/ 
+
+function alineaG(){
     let num1 = parseInt(prompt("Insira o primeiro valor:"))
     let num2 = parseInt(prompt("Insira o segundo valor:"))
-    output("Valores no intervalo [" + num1 + ", " + num2 + "[")
+    valoresIntervalo(num1, num2)
+}
+
+function valoresIntervalo(num1, num2){
+    let elemento = document.getElementById('output')
+    elemento.innerHTML += `Valores no intervalo [${num1}, ${num2}[ = `
     for(let i=num1; i<num2; i++){
-        console.log(i)
-        adicionarElementos(i)
+        outputMesmaLinha(i)
     }
 }
 
-function tabuada(){
+/*
+    ALÍNEA H
+*/
+
+function alineaH(){
     let num = parseInt(prompt("Qual a tabuada desejada?"))
-    console.log("Tabuada do " + num + ":")
-    output("Tabuada do " + num + ":")
+    tabuada(num)
+}
+
+function tabuada(num){
+    output(`Tabuada do ${num}:`)
     for(let i=1; i<=10; i++){
-        console.log(num + " x " + i + " = " + num*i)
-        adicionarElementos(num + " x " + i + " = " + num*i)
+        output(`${num} x ${i} = ${num*i}`)
     }
 }
 
-function multiplos3(){
+/*
+    ALÍNEA I
+*/
+
+function alineaI(){
     let num1 = parseInt(prompt("Insira o primeiro valor:"))
     let num2 = parseInt(prompt("Insira o segundo valor:"))
-    let soma = 0;
+    output(`Soma dos múltiplos de 3 no intervalo [${num1}, ${num2}] = ${multiplos3(num1, num2)}`)
+}
+
+function multiplos3(num1, num2){
+    let soma = 0
     for(let i=num1; i<=num2; i++){
         if(i % 3 == 0){
             soma += i
         }
     }
-    console.log("Soma dos múltiplos de 3 no intervalo [" + num1 + ", " + num2 + "] = " + soma)
-    output("Soma dos múltiplos de 3 no intervalo [" + num1 + ", " + num2 + "] = " + soma)
+    return soma
+}
 
+/*
+    ALÍNEA J
+*/
+
+function alineaJ(){
+    let num = parseInt(prompt("Insira um número:"))
+    let resultado = primo(num) ? `${num} é primo.` : `${num} não é primo.`
+    output(resultado)
 }
 
 function primo(){
@@ -142,27 +201,43 @@ function primo(){
         }
     }
     if(divisores <= 2 && num != 1){
-        console.log("Primo.")
-        output("Primo.")
+        return true
     }else{
-        console.log("Não primo.")
-        output("Não primo.")
+        return false
     }
 }
 
-function fatorial(){
+/*
+    ALÍNEA K
+*/
+
+function alineaK(){
     let num = parseInt(prompt("Insira um número:"))
+    output(`${num}! = ${fatorial(num)}`)
+}
+
+function fatorial(num){
     let fatorial = 1
     for(let i=1; i<=num; i++){
         fatorial *= i
     }
-    console.log(num + "! = " + fatorial)
-    output(num + "! = " + fatorial)
+    return fatorial
 }
 
-function perfeito(){
+/*
+    ALÍNEA L
+*/
+
+function alineaL(){
     let num = parseInt(prompt("Insira um número:"))
-    let divisores = [];
+    let resultado = isPerfeito(num) ? `${num} é perfeito! ${divisoresSoma(num)}` : `${num} não é perfeito.`
+    output(resultado)
+}
+
+let divisores
+
+function isPerfeito(num){
+    divisores = []
     let soma = 0
     for(let i=1; i<num; i++){
         if(num % i == 0){
@@ -171,54 +246,62 @@ function perfeito(){
         }
     }
     if(soma == num){
-        for(let i=0; i<divisores.length - 1; i++){
-            adicionarElementos(divisores[i])
-        }
-        adicionarElementos("+ " + divisores[divisores.length - 1])
-        adicionarElementos("---------------")
-        adicionarElementos(num)
-        console.log("Número perfeito.")
-        output("Número perfeito:")
+        return true
     }else{
-        console.log("Não é perfeito.")
-        output("Não é perfeito.")
+        return false
     }
 }
 
-function bissexto(){   
-    let bissexto = false
+function divisoresSoma(num){
+    let string = ""
+    for (let i=0; i<divisores.length - 1; i++){
+        string += divisores[i] + " + "
+    }
+    string += divisores[divisores.length - 1] + " = " + num
+    return string
+}
+
+/*
+    ALÍNEA M
+*/
+
+function alineaM(){
     let ano = parseInt(prompt("Insira o ano desejado:"))
-    if(ano % 4 == 0 && ano % 100 == 0 && ano % 400 == 0){
-        bissexto = true
-    }else if(ano % 4 == 0 && ano % 100 == 0){
-        bissexto = false
-    }else if(ano % 4 == 0){
-        bissexto = true
-    }
+    let resultado = isBissexto(ano) ? `O ano ${ano} é bissexto.` : `O ano ${ano} não é bissexto.`
+    output(resultado)
+}
 
-    if(bissexto){
-        console.log("O ano " + ano + " é bissexto.")
-        output("O ano " + ano + " é bissexto.")
+function isBissexto(ano){   
+    if((ano % 4 == 0 && ano % 100 != 0 ) || (ano % 400 == 0)){
+        return true
     }else{
-        console.log("O ano " + ano + " não é bissexto.")
-        output("O ano " + ano + " é bissexto.")
+        return false
     }
 }
 
-function capicua(){
-    let num
+/*
+    ALÍNEA N
+*/
+
+function alineaN(){
     do{
         num = prompt("Insira um número entre 100 e 999 (inclusive):")
     } while(num < 100 || num > 999)
+    let resultado = isCapicua(num) ? `O número ${num} é capicua.` : `O número ${num} não é capicua.`
+    output(resultado)
+}
 
+function isCapicua(num){
     if(num.charAt(0) == num.charAt(2)){
-        console.log("O número " + num + " é capicua.")
-        output("O número " + num + " é capicua.")
+        return true
     }else{
-        console.log("O número " + num + " não é capicua.")
-        output("O número " + num + " não é capicua.")
+        return false
     }
 }
+
+/*
+    ALÍNEA O
+*/
 
 function advinha(){
     let aleatorio = Math.floor(Math.random(1) * 100)
@@ -228,13 +311,12 @@ function advinha(){
     do {
         num = parseInt(prompt("Insira um valor (" + (6-tentativas) + " tentativas restantes):"))
         if(num > aleatorio){
-            console.log("Insira um valor menor!")
+            output("Insira um valor menor!")
             alert("Insira um valor menor!")
         }else if(num < aleatorio){
-            console.log("Insira um valor maior!")
+            output("Insira um valor maior!")
             alert("Insira um valor maior!")
         }else{
-            console.log("Parabéns, advinhou o número! Tentativas utilizadas: " + tentativas)
             output("Parabéns, advinhou o número! Tentativas utilizadas: " + tentativas)
         }
         tentativas++
